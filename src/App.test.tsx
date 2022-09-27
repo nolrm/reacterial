dd9d86e3bff67009519a319dbda576d3d43b512a
Marlon Maniti <lonmaniti@gmail.com>
2022-09-27 22:55:02 +1300

    Updates static title and descriptionnpm start

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('renders description', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const desc = screen.getByText(/React \+ Material UI/i);
+  expect(desc).toBeInTheDocument();
 });
