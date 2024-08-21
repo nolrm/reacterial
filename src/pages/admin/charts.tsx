8dc8d8514ab078130b533d0d6e5eb0c96ffacc83
Marlon Maniti <Marlon.maniti@fostermoore.com>
2024-08-21 21:54:38 +1200

    Add charts

--- src/pages/admin/charts.tsx
+++ src/pages/admin/charts.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import LayoutAdmin from '@/components/layout-admin';
 import Typography from '@mui/material/Typography';
-// import { BarChart } from '@mui/x-charts/BarChart';
-
+import BarChartComponent from '@/components/BarChartComponent';
+import LineChartComponent from '@/components/LineChartComponent';
 
 const ChartsPage: React.FC = () => {
     return (
@@ -10,17 +10,8 @@ const ChartsPage: React.FC = () => {
             <Typography variant="h4" component="h1" gutterBottom>
                 Charts
             </Typography>
-            {/*<BarChart*/}
-            {/*    series={[*/}
-            {/*        { data: [35, 44, 24, 34] },*/}
-            {/*        { data: [51, 6, 49, 30] },*/}
-            {/*        { data: [15, 25, 30, 50] },*/}
-            {/*        { data: [60, 50, 15, 25] },*/}
-            {/*    ]}*/}
-            {/*    height={290}*/}
-            {/*    xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}*/}
-            {/*    margin={{ top: 10, bottom: 30, left: 40, right: 10 }}*/}
-            {/*/>*/}
+            <BarChartComponent/>
+            <LineChartComponent/>
         </LayoutAdmin>
     );
 };
